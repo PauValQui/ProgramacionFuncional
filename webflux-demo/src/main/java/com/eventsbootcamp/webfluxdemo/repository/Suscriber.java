3e4528032c88dc3f8298f561d74c1be97fc9028b
Paulavq <91205671+PauValQui@users.noreply.github.com>
2022-10-06 17:03:34 +0200

    webflux-demo

---
+++ webflux-demo/src/main/java/com/eventsbootcamp/webfluxdemo/repository/Suscriber.java
@@ -0,0 +1,7 @@
+package com.eventsbootcamp.webfluxdemo.repository;
+
+public class Suscriber {
+	public static void print(Integer n) {
+		System.out.println("Subscriber 1: "+n);
+	}
+}
---
+++ webflux-demo/src/test/java/com/eventsbootcamp/webfluxdemo/WebfluxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventsbootcamp.webfluxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
